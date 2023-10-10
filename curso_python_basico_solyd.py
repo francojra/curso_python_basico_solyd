56b1f8461d2535f65ec7f7cc06df997269e1470d
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-10-09 23:47:09 -0300

    Add files via upload

--- curso_python_basico_solyd.py
+++ curso_python_basico_solyd.py
@@ -9,5 +9,9 @@
 ### Linguagem surgiu em 1989, é de alto nível e orientada a objetos. É uma das principais
 ### linguagens do mundo, de script e interpretada. Apresenta elementos funcionais com
 ### tipagem (tipos de variáveis) dinâmica e forte (inferida). Apresenta uma comunidade 
-### rica com muitas bibliotecas. O Python é usado para análise de dados, produção de sites
-### e jogos.
+### rica com muitas bibliotecas. O Python é usado para análise de dados, produção de sites,
+### jogos, segurança da informação, matemática, etc. É possível produzir com poucas linhas
+### de comando.
+
+### O Python é usado por grandes empresas nacionais e internacionais como Google, YouTube,
+### Nasa, Linux, IBM, Apple, Rede Globo, Microsoft, etc.
