83aeb83b9e10c79c8b3d9c497255c00235df7bb7
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-10-09 23:43:50 -0300

    Add files via upload

--- curso_python_basico_solyd.py
+++ curso_python_basico_solyd.py
@@ -4,3 +4,10 @@
 # Data: 09/10/23 ---------------------------------------------------------------------------------------------------------------------------
 # Referência: Curso Solyd ------------------------------------------------------------------------------------------------------------------
 
+# Introdução ao Python
+
+### Linguagem surgiu em 1989, é de alto nível e orientada a objetos. É uma das principais
+### linguagens do mundo, de script e interpretada. Apresenta elementos funcionais com
+### tipagem (tipos de variáveis) dinâmica e forte (inferida). Apresenta uma comunidade 
+### rica com muitas bibliotecas. O Python é usado para análise de dados, produção de sites
+### e jogos.
