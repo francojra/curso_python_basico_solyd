05c6c415caf6c7fe6014a2ee051735d65df3e334
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-10-17 22:42:49 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from veiculo import Veiculo
 
-caminhao_rosa = Veiculo('rosa', 'Ford', 6) # Cor, nome da marca e número de rodas
+caminhao_rosa = Veiculo('rosa', 6, 'Ford') # Cor, nome da marca e número de rodas
 print(caminhao_rosa)
 print(type(caminhao_rosa)) # Criou um novo tipo de classe
+
+### Agora podemos criar objetos do tipo 'veículo'
+
+### Veja as opções que aparecem ao digitar 'caminhao_rosa.'
+
+caminhao_rosa.
+
+print(caminhao_rosa.cor)
+print(caminhao_rosa.rodas)
+print(caminhao_rosa.marca)
