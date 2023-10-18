ae16cec653ef7e3f187e2c4d4314c337803dd402
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-10-17 23:21:19 -0300

    Add files via upload

---
+++ cliente.py
@@ -0,0 +1,6 @@
+class Cliente:
+  def __init__(self, nome, idade, empresa, salario): # Método construtor, constroi o objeto
+      self.nome = nome
+      self.idade = idade
+      self.empresa = empresa
+      self.salario = salario
