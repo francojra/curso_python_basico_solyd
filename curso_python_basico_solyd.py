a673bf6523cea7cd913fdb18774bc387639ea393
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-10-22 23:49:08 -0300

    Add files via upload

--- curso_python_basico_solyd.py
+++ curso_python_basico_solyd.py
@@ -883,3 +883,4 @@ print('Arquivo aberto')
   
 # Bibliotecas, PIP e requisições web ------------------------------------------------------------------------------------------------------------
 
+### Instalações de pacotes externos
