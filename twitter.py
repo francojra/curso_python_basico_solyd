5878b0753d18647f67c2a237823b8a354d69be61
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-11-09 22:13:42 -0300

    Add files via upload

--- twitter.py
+++ twitter.py
@@ -3,3 +3,13 @@
 import oauth2
 
 class Twitter:
+  
+  def __init__(self, consumer_key, cosumer_secret, token_key, token_secret):
+      self.conexao(consumer_key, cosumer_secret, token_key, token_secret)
+    
+  def conexao(self, consumer_key, cosumer_secret, token_key, token_secret):
+      self.consumer = oauth2.Consumer(consumer_key, cosumer_secret)
+      self.token = oauth2.Token(token_key, token_secret)
+      self.cliente = oauth2.Client(self.consumer, self.token)
+      return self.cliente
+ 
