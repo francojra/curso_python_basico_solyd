7d1713084c6339cdaddbe8f65086d6f0a3eddf01
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-11-09 22:31:33 -0300

    Add files via upload

--- main-twitter.py
+++ main-twitter.py
@@ -9,3 +9,9 @@ token_secret =
 twitter = Twitter(consumer_key, cosumer_secret, token_key, token_secret)
 
 print(twitter) # Retorna a conexão
+
+### Função para fazer postagens no Twitter
+
+twitter.tweet('Vamos testar nossa lib') # Colocar entre aspas o que quer postar
+
+### A farse 'Vamos postar nossa lib' aparecerá automaticamente no Twitter.
